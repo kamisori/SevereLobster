d62cfcb046ad317a87552b13bc8b760d4481d330
unknown <scla@NA716SCLA1.soptim.net>
2012-09-29 09:02:48 +0200

    Erste Projektstruktur mit main (testweise)

---
+++ src/main/java/StartApplication.java
@@ -0,0 +1,15 @@
+/**
+ * Test-Main
+ *
+ * @author scla
+ */
+public class StartApplication {
+
+    /**
+     * Main Methode
+     * @param args Argumente
+     */
+    public static void main(String[] args){
+        System.out.println("Ausgabe aus der main()-Methode");
+    }
+}
