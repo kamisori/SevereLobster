594ecbcb7efe55d910dd90f1a28084d6172351c0
lutzKleiber <lutz.kleiber@gmx.net>
2012-10-18 19:15:18 +0200

    Gedrehte Pfeilicons. Vorerst fremde Implementation.

--- projekt/src/main/java/severeLobster/frontend/view/IconFactory.java
+++ projekt/src/main/java/severeLobster/frontend/view/IconFactory.java
@@ -1,5 +1,6 @@
 package severeLobster.frontend.view;
 
+import infrastructure.constants.enums.PfeilrichtungEnumeration;
 import infrastructure.graphics.icons.IIconPackage;
 import severeLobster.backend.spiel.Ausschluss;
 import severeLobster.backend.spiel.Pfeil;
@@ -33,8 +34,34 @@ public abstract class IconFactory {
             return iconPackage.ausschlussIcon();
         }
         if (state instanceof Pfeil) {
-            //TODO Hier zwischen versch. Richtungen unterscheiden
-            return iconPackage.pfeilIcon();
+            // TODO Hier zwischen versch. Richtungen unterscheiden
+
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.SUED) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 0);
+            }
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.SUEDWEST) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 45);
+            }
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.WEST) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 90);
+            }
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.NORDWEST) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 135);
+            }
+
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.NORD) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 180);
+            }
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.NORDOST) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 225);
+            }
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.OST) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 270);
+            }
+            if (((Pfeil) state).getPfeilrichtung() == PfeilrichtungEnumeration.SUEDOST) {
+                return new RotatedIcon(iconPackage.pfeilIcon(), 315);
+            }
+
         }
         return iconPackage.blankIcon();
     }
