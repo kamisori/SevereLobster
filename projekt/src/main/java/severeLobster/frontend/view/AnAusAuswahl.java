698c92c54d0e9d80f0e9dae21f8acca90e3378aa
Lars Schlegelmilch <ebbe@zweifall-online.de>
2012-11-02 17:26:36 +0100

    Tracking-Auswahl refactoring

--- projekt/src/main/java/severeLobster/frontend/view/AnAusAuswahl.java
+++ projekt/src/main/java/severeLobster/frontend/view/AnAusAuswahl.java
@@ -3,34 +3,28 @@ package severeLobster.frontend.view;
 import infrastructure.ResourceManager;
 import infrastructure.constants.GlobaleKonstanten;
 
-import javax.swing.Icon;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import java.awt.Color;
-import java.awt.Insets;
-import java.awt.event.MouseListener;
+import javax.swing.*;
+import java.awt.*;
 
 /**
- * An-Auswahl fuer den Tracking-Modus
+ * An/Auswahl fuer den Tracking-Modus
+ *
  * @author Lars Schlegelmilch
  */
-public class AnAusAuswahl extends JCheckBox
-{
+public class AnAusAuswahl extends JCheckBox {
     private final ResourceManager resourceManager = ResourceManager.get();
-    private JLabel label;
-    private MouseListener mouseListener;
-    private Icon icon = resourceManager.getImageIcon("switch-off.png");
-    private Icon selected = resourceManager.getImageIcon("switch-on.png");
 
-    public AnAusAuswahl(boolean visible)
-    {
+    private Icon icon = resourceManager.getImageIcon("switch-off.png");
+    private Icon iconSelected = resourceManager.getImageIcon("switch-on.png");
+
+    public AnAusAuswahl(boolean visible) {
         super.setFont(GlobaleKonstanten.FONT.deriveFont((float) 17));
         super.setFocusable(false);
-        super.setText("Tracking-Modus");
+        super.setText(resourceManager.getText("tracking.an.aus.text"));
         super.setForeground(Color.YELLOW);
         super.setHorizontalTextPosition(2);
         super.setIcon(icon);
-        super.setSelectedIcon(selected);
+        super.setSelectedIcon(iconSelected);
         super.setContentAreaFilled(false);
         super.setMargin(new Insets(0, 0, 0, 0));
         super.setSelected(false);
