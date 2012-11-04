fdc55d0eca4c83298cecbc5f9edd8ef531a5dbbe
sfrings <stefan-frings@arcor.de>
2012-11-04 15:20:53 +0100

    auskommentierte Zeile nicht geloescht

--- projekt/src/main/java/infrastructure/exceptions/UngueltigeSpielfeldgroessenangabeException.java
+++ projekt/src/main/java/infrastructure/exceptions/UngueltigeSpielfeldgroessenangabeException.java
@@ -13,7 +13,6 @@ public class UngueltigeSpielfeldgroessenangabeException extends
     private static final ResourceManager resourceManager = ResourceManager.get();
 
     public UngueltigeSpielfeldgroessenangabeException() {
-        //super("Die Groessenangabe des Spielfeldes ist ungueltig!");
         super(resourceManager.getText("exception.size.of.playing.field.invalid"));
     }
 }
