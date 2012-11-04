399d0c4e10f9ea90511c285df6b004680c084ef6
J.-Fabian Wenisch <fwenisch@nirako.de>
2012-11-04 18:44:16 +0100

    Mainview refactored
    Für das einfache laden der Spiele, "Spielfeld laden" ausgelagert

--- projekt/src/main/java/severeLobster/frontend/view/MainView.java
+++ projekt/src/main/java/severeLobster/frontend/view/MainView.java
@@ -18,6 +18,7 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainView extends JPanel {
@@ -30,13 +31,41 @@ public class MainView extends JPanel {
 		return backend;
 	}
 
-	private final SternenSpielApplicationBackend backend;
+	private SternenSpielApplicationBackend backend;
 
-	public MainView() throws IOException {
+	public MainView() throws IOException 
+	{
 
+		
+		
+		add(getNewSpielfeld("Standardspiel01"));
+		
+		
+		setVisible(true);
+	}
+/**
+ * Erstellt ein neues SpiefeldPanel anhand des übergebenen PuzzleNamens aus dem Resource Ordner
+ * @param strPuzzleName - Der Name des zu ladenen Puzzles (ohne ".puz")
+ * @return - JPanel auf dem das Spielfeld und das SpielInfo panel zu sehen sind
+ * @author fwenisch
+ * @date 04.11.2012
+ */
+	public JPanel getNewSpielfeld(String strPuzzleName)
+	{
 		backend = new SternenSpielApplicationBackend();
-		backend.startNewSpielFrom("Standardspiel01");
-
+		try 
+		{
+			backend.startNewSpielFrom(strPuzzleName);
+		} catch (FileNotFoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JPanel Spielfeld = new JPanel();
 		final SpielfeldDarstellung spielfeldView = new SpielfeldDarstellung();
 		new SpielfeldDarstellungsSteuerung(spielfeldView, backend);
 		final SpielmodusViewPanel spielmodusView = new SpielmodusViewPanel();
@@ -47,53 +76,10 @@ public class MainView extends JPanel {
 				backend);
 		trackingView.setTrackingControllViewController(trackingViewCtrl);
 		JPanel spielinfo = new SpielinfoView(trackingView);
-		add(spielfeldView, BorderLayout.CENTER);
-		add(spielinfo, BorderLayout.EAST);
-		setVisible(true);
-	}
-
-	public MainView(final Spiel spiel) throws IOException {
-		// TODO: Layout wird zwecks Platzhalter auf null gesetzt -> Layout!
-		setLayout(null);
-		// JPanel spielfeld = new SpielfeldView_fwenisch();
-		/**
-		 * Nur zum Testen
-		 */
-		backend = new SternenSpielApplicationBackend();
-		// backend.startNewSpielFrom("Standardspiel01");
-		backend.setSpiel(spiel);
-		/*
-		 * backend.getSpiel().initializeNewSpielfeld(20, 18);
-		 * backend.getSpiel().setSpielmodus(SpielmodusEnumeration.EDITIEREN);
-		 */
-		final SpielfeldDarstellung view = new SpielfeldDarstellung();
-		new SpielfeldDarstellungsSteuerung(view, backend);
-
-		final SpielmodusViewPanel spielmodusView = new SpielmodusViewPanel();
-
-		new SpielmodusViewController(spielmodusView, backend);
-
-		JPanel spielfeld = new JPanel(false);
-		spielfeld.setOpaque(false);
-		spielfeld.setLayout(new BoxLayout(spielfeld, BoxLayout.Y_AXIS));
-		spielfeld.add(view);
-		// spielfeld.add(spielmodusView);
-
-		/**
-		 * Ende Test
-		 */
-
-		spielfeld.setBounds(50, 50, 500, 500);
-		final TrackingControllView trackingView = new TrackingControllView();
-		final TrackingControllViewController trackingViewCtrl = new TrackingControllViewController(
-				backend);
-		trackingView.setTrackingControllViewController(trackingViewCtrl);
-
-		JPanel spielinfo = new SpielinfoView(trackingView);
-		spielinfo.setBounds(550, 50, 200, 500);
-		add(spielfeld);
-		add(spielinfo);
-		setVisible(true);
+		Spielfeld.add(spielfeldView, BorderLayout.CENTER);
+		Spielfeld.add(spielinfo, BorderLayout.EAST);
+		Spielfeld.setOpaque(false);
+		return Spielfeld;
 	}
 
 	@Override
