c07012a31a679ccd43882d749a9b8472a2b2060f
Christian Lobach <Christian.Lobach@gmail.com>
2012-11-23 12:49:30 +0100

    SolvingStepExcludeBehindInColumn und SolvingStepExcludeBehindInRow inkl. Tests hinzugefügt

--- projekt/src/test/java/severeLobster/backend/spiel/Loesungs_Strategien_Test.java
+++ projekt/src/test/java/severeLobster/backend/spiel/Loesungs_Strategien_Test.java
@@ -2,6 +2,7 @@ package severeLobster.backend.spiel;
 
 import infrastructure.constants.enums.SpielmodusEnumeration;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -151,8 +152,52 @@ public class Loesungs_Strategien_Test implements IGotSpielModus {
         assertEquals(Ausschluss.getInstance(), spielfeld.getSpielstein(1,3));
         assertEquals(Ausschluss.getInstance(), spielfeld.getSpielstein(2,3));
         assertEquals(Ausschluss.getInstance(), spielfeld.getSpielstein(3,3));
+    }
+
+    @Test
+    public void ein_Stern_ein_Pfeil_in_Spalte(){
+        // Pfeile im Editiermodus setzen
+        spielfeld.setSpielstein(1, 0, Pfeil.getOstPfeil());
+        spielfeld.setSpielstein(0, 1, Pfeil.getSuedPfeil());
+        spielfeld.setSpielstein(3, 1, Pfeil.getSuedWestPfeil());
+
+        // GotSpielmodus setzen, damit Spielmodus LOESEN ist
+        spielfeld.setGotSpielModus(this);
+
+        // Sterne setzen
+        spielfeld.setSpielstein(2,0, Stern.getInstance());
+        spielfeld.setSpielstein(0,2, Stern.getInstance());
+        spielfeld.setSpielstein(2,2, Stern.getInstance());
+
+        SolvingStep step = new SolvingStepExcludeBehindInColumn();
+        spielfeld = step.execute(spielfeld);
+
+        assertEquals(Ausschluss.getInstance(), spielfeld.getSpielstein(0,0));
 
     }
+
+    @Test
+    public void ein_Stern_ein_Pfeil_in_Zeile(){
+        // Pfeile im Editiermodus setzen
+        spielfeld.setSpielstein(1, 0, Pfeil.getOstPfeil());
+        spielfeld.setSpielstein(0, 1, Pfeil.getSuedPfeil());
+        spielfeld.setSpielstein(3, 1, Pfeil.getSuedWestPfeil());
+
+        // GotSpielmodus setzen, damit Spielmodus LOESEN ist
+        spielfeld.setGotSpielModus(this);
+
+        // Sterne setzen
+        spielfeld.setSpielstein(2,0, Stern.getInstance());
+        spielfeld.setSpielstein(0,2, Stern.getInstance());
+        spielfeld.setSpielstein(2,2, Stern.getInstance());
+
+        SolvingStep step = new SolvingStepExcludeBehindInRow();
+        spielfeld = step.execute(spielfeld);
+
+        assertEquals(Ausschluss.getInstance(), spielfeld.getSpielstein(0,0));
+
+    }
+
 
     @Override
     public SpielmodusEnumeration getSpielmodus() {
