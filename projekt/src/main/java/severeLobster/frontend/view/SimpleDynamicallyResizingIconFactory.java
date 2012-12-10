3486d8165271e7651f5a17a06cbbcef57eb6c381
lutzKleiber <lutz.kleiber@gmx.net>
2012-12-10 13:31:01 +0100

    Bugfix (groesstenteils): Bei grossen Spielfeldern verschwindet nun beim setzen eines Steins nicht mehr das Infopanel.

--- projekt/src/main/java/severeLobster/frontend/view/SimpleDynamicallyResizingIconFactory.java
+++ projekt/src/main/java/severeLobster/frontend/view/SimpleDynamicallyResizingIconFactory.java
@@ -10,33 +10,37 @@ package severeLobster.frontend.view;
 public class SimpleDynamicallyResizingIconFactory extends SimpleIconFactory {
 
     private static final SimpleDynamicallyResizingIconFactory INSTANCE = new SimpleDynamicallyResizingIconFactory();
-    public static final int DEFAULT_ICON_WIDTH = 24;
-    public static final int DEFAULT_ICON_HEIGHT = 24;
+    public static final int DEFAULT_ICON_WIDTH = 4;
+    public static final int DEFAULT_ICON_HEIGHT = 4;
 
     protected SimpleDynamicallyResizingIconFactory() {
 
-//        sternIcon = new DynamischSkalierendesIcon(sternIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        ausschlussIcon = new DynamischSkalierendesIcon(ausschlussIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        blankIcon = new DynamischSkalierendesIcon(blankIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilSouthIcon = new DynamischSkalierendesIcon(pfeilSouthIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilSouthWestIcon = new DynamischSkalierendesIcon(pfeilSouthWestIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilWestIcon = new DynamischSkalierendesIcon(pfeilWestIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilNorthWestIcon = new DynamischSkalierendesIcon(pfeilNorthWestIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilNorthIcon = new DynamischSkalierendesIcon(pfeilNorthIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilNorthEastIcon = new DynamischSkalierendesIcon(pfeilNorthEastIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilEastIcon = new DynamischSkalierendesIcon(pfeilEastIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
-//        pfeilSouthEastIcon = new DynamischSkalierendesIcon(pfeilSouthEastIcon,
-//                DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // sternIcon = new DynamischSkalierendesIcon(sternIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // ausschlussIcon = new DynamischSkalierendesIcon(ausschlussIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // blankIcon = new DynamischSkalierendesIcon(blankIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilSouthIcon = new DynamischSkalierendesIcon(pfeilSouthIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilSouthWestIcon = new
+        // DynamischSkalierendesIcon(pfeilSouthWestIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilWestIcon = new DynamischSkalierendesIcon(pfeilWestIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilNorthWestIcon = new
+        // DynamischSkalierendesIcon(pfeilNorthWestIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilNorthIcon = new DynamischSkalierendesIcon(pfeilNorthIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilNorthEastIcon = new
+        // DynamischSkalierendesIcon(pfeilNorthEastIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilEastIcon = new DynamischSkalierendesIcon(pfeilEastIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
+        // pfeilSouthEastIcon = new
+        // DynamischSkalierendesIcon(pfeilSouthEastIcon,
+        // DEFAULT_ICON_WIDTH, DEFAULT_ICON_HEIGHT);
     }
 
     public static SimpleDynamicallyResizingIconFactory getInstance() {
